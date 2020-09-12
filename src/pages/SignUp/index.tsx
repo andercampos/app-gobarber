1d71d4fb7a82dcd211549dd7c7e40e8312fd21f2
Anderson Campos <anderleitec@gmail.com>
2020-09-12 16:36:11 -0300

    SignUp connected with API

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import * as Yup from 'yup';
 
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -53,9 +54,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer logon na aplicação',
+      );
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
@@ -130,7 +136,7 @@ const SignUp: React.FC = () => {
 
               <View>
                 <Button onPress={() => formRef.current?.submitForm()}>
-                  Entrar
+                  Cadastrar
                 </Button>
               </View>
             </Form>
